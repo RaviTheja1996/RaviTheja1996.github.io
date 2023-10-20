b9ea86f9650c936bdc5420979da4af673e6cc9a3
Ravi Theja <kopparapuraviteja@gmail.com>
2023-10-20 19:13:48 +0530

    github claendar updated

--- src/container/Github/Github.jsx
+++ src/container/Github/Github.jsx
@@ -6,10 +6,25 @@ const Github = () => {
     <div style={{ display: "flex", flexDirection: "column" }}>
       <h2 className='head-text' style={{ marginTop: "4rem" }}><span>Github</span> Stats</h2>
       <div style={{ display: "flex", justifyContent: "center", marginTop: "3rem", flexDirection: "column", gap: "2rem" }}>
-        <a href="https://git.io/streak-stats" target='_blank' rel='noreferrer' style={{ margin: "auto" }} id='github-streak-stats'><img src="https://github-readme-streak-stats.herokuapp.com?user=RaviTheja1996&theme=graywhite&border_radius=15&card_width=600" alt="GitHub Streak" /></a>
-        <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=RaviTheja1996&layout=pie" id='github-top-langs' style={{ height: "30rem" }} alt=""></img>
+        <a
+          href="https://git.io/streak-stats"
+          target='_blank'
+          rel='noreferrer'
+          style={{ margin: "auto" }}
+          id='github-streak-stats'>
+          <img
+            src="https://github-readme-streak-stats.herokuapp.com?user=RaviTheja1996&theme=graywhite&border_radius=15&card_width=600"
+            alt="GitHub Streak" />
+        </a>
+        <img
+          src="https://github-readme-stats.vercel.app/api/top-langs/?username=RaviTheja1996&layout=pie"
+          id='github-top-langs'
+          style={{ height: "30rem" }}
+          alt=""></img>
         <img src="https://github-readme-stats.vercel.app/api?username=RaviTheja1996&hide=contribs,prs" id='github-stats-card' alt=""></img>
-        <div className='react-activity-calendar'></div>
+        <div className='react-activity-calendar'>
+          <img src="https://ghchart.rshah.org/313bac/ravitheja1996" alt="github-activity-calendar" />
+        </div>
       </div>
     </div>
   )
