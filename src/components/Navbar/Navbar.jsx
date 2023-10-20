6551bc318d18e3e8a03fecded684d69dc8ed6a1e
Ravi Theja <kopparapuraviteja@gmail.com>
2023-10-20 17:08:51 +0530

    making some changes according to test log on op

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,46 +8,48 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className='app__navbar' id="nav-menu">
-      <div className='app__navbar-logo'>
-        <img src={images.raviLogo} alt="logo" />
-      </div>
-      <ul className='app__navbar-links'>
-        {['home', 'about', 'projects', 'skills', 'contact'].map((item) => (
-          <li className='app__flex p-text' key={`link-${item}`}>
+    <div id="nav-menu">
+      <nav className='app__navbar'>
+        <div className='app__navbar-logo'>
+          <img src={images.raviLogo} alt="logo" />
+        </div>
+        <ul className='app__navbar-links'>
+          {['home', 'about', 'projects', 'skills', 'contact'].map((item) => (
+            <li className='app__flex p-text' key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`} className={`nav-link ${item}`}>{item}</a>
+            </li>
+          ))}
+          <li className='app__flex p-text'>
             <div />
-            <a href={`#${item}`} className={`nav-link ${item}`}>{item}</a>
+            <a href={`#contact`} className='nav-link resume'>{"resume"}</a>
           </li>
-        ))}
-        <li className='app__flex p-text'>
-          <div />
-          <a href={`#contact`} className='nav-link resume'>{"resume"}</a>
-        </li>
-      </ul>
-      <div className='app__navbar-menu'>
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
-        {
-          toggle && (
-            <motion.div
-              whileInView={{ x: [300, 0] }}
-              transition={{ duration: 0.7, ease: "easeOut" }}
-            >
-              <HiX onClick={() => setToggle(false)} />
-              <ul>
-                {['home', 'about', 'projects', 'skills', 'contact'].map((item) => (
-                  <li key={item}>
-                    <a href={`#${item}`} className={`nav-link ${item}`} onClick={() => setToggle(false)}>{item}</a>
+        </ul>
+        <div className='app__navbar-menu'>
+          <HiMenuAlt4 onClick={() => setToggle(true)} />
+          {
+            toggle && (
+              <motion.div
+                whileInView={{ x: [300, 0] }}
+                transition={{ duration: 0.7, ease: "easeOut" }}
+              >
+                <HiX onClick={() => setToggle(false)} />
+                <ul>
+                  {['home', 'about', 'projects', 'skills', 'contact'].map((item) => (
+                    <li key={item}>
+                      <a href={`#${item}`} className={`nav-link ${item}`} onClick={() => setToggle(false)}>{item}</a>
+                    </li>
+                  ))}
+                  <li className='app__flex p-text nav-link resume'>
+                    <a href={`#contact`}>{"resume"}</a>
                   </li>
-                ))}
-                <li className='app__flex p-text nav-link resume'>
-                  <a href={`#contact`}>{"resume"}</a>
-                </li>
-              </ul>
-            </motion.div>
-          )
-        }
-      </div>
-    </nav>
+                </ul>
+              </motion.div>
+            )
+          }
+        </div>
+      </nav>
+    </div>
   )
 }
 
