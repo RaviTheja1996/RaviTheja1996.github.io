05fe5fedf8d6d1b8431a16311c959644279c7720
Ravi Theja <kopparapuraviteja@gmail.com>
2023-10-21 00:05:13 +0530

    working on github section

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -48,11 +48,8 @@ const About = () => {
         <h2 className='head-text' style={{ fontSize: "2.5rem" }}>
           I know that <span>Good Development</span> means <span>Good Business</span>
         </h2>
-        {/* <div id='user-detail-intro'>
-          A dedicated full stack web developer, I bring passion, lifelong learning, and proficiency in the MERN stack to the table. My focus is on crafting robust, functional, and user-centric websites having attention to detail and a problem-solving mindset my work is to deliver seamless and dynamic web experiences. With hands-on experience and adeptness in Git for version control. Quality and solution driven, skilled in and having hands-on experience of working with HTML, CSS, JavaScript, React, NodeJS and MongoDB. Highly resilient and focused on learning new techniques to build real world websites while facilitating the growth of self and that of the organization.
-        </div> */}
         <p id='user-detail-intro'>
-          Quality and solution driven, skilled in and having hands-on experience of working majorly with HTML, CSS, JavaScript, React. Highly resilient and focused on learning new techniques to build real world websites while facilitating the growth of self and that of the organization.
+          A dedicated full stack web developer, I bring passion, lifelong learning, and proficiency in the MERN stack to the table. My focus is on crafting robust, functional, and user-centric websites having attention to detail and a problem-solving mindset with hands-on experience on Git for version control. Quality and solution driven, skilled in and having hands-on experience of working with HTML, CSS, JavaScript, React, NodeJS and MongoDB. Highly resilient and focused on learning new techniques to build real world websites while facilitating the growth of self and that of the organization.
         </p>
       </div>
       <div className='app__profiles'>
