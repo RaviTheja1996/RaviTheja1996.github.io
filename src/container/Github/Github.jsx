d76071b27e472ce753ea99653de70f66b5d1c499
Ravi Theja <kopparapuraviteja@gmail.com>
2023-10-21 02:22:44 +0530

    working on github section

--- src/container/Github/Github.jsx
+++ src/container/Github/Github.jsx
@@ -10,20 +10,26 @@ const Github = () => {
         <div className='flex-item'>
           <img
             id='github-streak-stats'
+            style={{ maxWidth: "100%" }}
             src="https://github-readme-streak-stats.herokuapp.com?user=RaviTheja1996&theme=graywhite&border_radius=15&card_width=600"
             alt="GitHub Streak" />
         </div>
         <div className='flex-item'>
           <img
-            src="https://github-readme-stats.vercel.app/api?username=RaviTheja1996&hide=contribs,prs" id='github-stats-card'
+            src="https://github-readme-stats.vercel.app/api?username=RaviTheja1996&hide=contribs,prs"
+            style={{ maxWidth: "100%" }}
+            id='github-stats-card'
             alt="" />
         </div>
         <div className='react-activity-calendar flex-item'>
-          <img src="https://ghchart.rshah.org/313bac/ravitheja1996" alt="github-activity-calendar" />
+          <img src="https://ghchart.rshah.org/313bac/ravitheja1996"
+            style={{ maxWidth: "100%" }}
+            alt="github-activity-calendar" />
         </div>
         <div className='flex-item'>
           <img
             src="https://github-readme-stats.vercel.app/api/top-langs/?username=RaviTheja1996&layout=donut"
+            style={{ maxWidth: "100%" }}
             id='github-top-langs'
             alt="" />
         </div>
