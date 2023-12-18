714a24e0828066da386b68465aa92b9325b1d8ef
RaviTheja1996 <kopparapuraviteja@gmail.com>
2023-12-18 18:59:35 +0530

    navbar resume download change

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -32,7 +32,16 @@ const Navbar = () => {
             }}
           >
             <img src={images.downloadIcon} alt="downloadIcon" />
-            <a id="resume-link-1" href={`#contact`}>
+            <a
+              id="resume-link-1"
+              download="Ravitheja-Kopparapu-Resume.pdf"
+              href="https://drive.google.com/file/d/1ye18h6I5gIOY4hW4E4QxovyhaaxYsm5h/view?usp=drive_link"
+              // href="https://1drv.ms/b/s!AhKK9bxRRzY_pzyaZ1aRdagWh87U?e=nICvJC"
+              // href={images.resumePdf}
+              // href="https://rb.gy/k7ekc0"
+              target="_blank"
+              rel="noreferrer"
+            >
               {"Resume"}
             </a>
           </button>
@@ -65,14 +74,19 @@ const Navbar = () => {
                   id="resume-button-1"
                   className="nav-link resume app__flex"
                   onClick={() => {
-                    setTimeout(() => { window.open(`${images.resumePdf}`, "_blank"); }, 100);
+                    setTimeout(() => {
+                      window.open(`${images.resumePdf}`, "_blank");
+                    }, 100);
                   }}
                 >
                   <img src={images.downloadIcon} alt="downloadIcon" />
                   <a
                     id="resume-link-1"
                     download="Ravitheja-Kopparapu-Resume.pdf"
-                    href="https://drive.google.com/file/d/1ye18h6I5gIOY4hW4E4QxovyhaaxYsm5h/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1ye18h6I5gIOY4hW4E4QxovyhaaxYsm5h/view?usp=drive_link"
+                    // href="https://1drv.ms/b/s!AhKK9bxRRzY_pzyaZ1aRdagWh87U?e=nICvJC"
+                    // href={images.resumePdf}
+                    // href="https://rb.gy/k7ekc0"
                     target="_blank"
                     rel="noreferrer"
                   >
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,32 +1,32 @@
 /* eslint-disable import/no-anonymous-default-export */
-import email from '../assets/email.png';
-import mobile from '../assets/mobile-1.png';
-import api from '../assets/api.png';
-import cpp from '../assets/cpp.png';
-import css from '../assets/css.png';
-import figma from '../assets/figma.png';
-import flutter from '../assets/flutter.png';
-import git from '../assets/git.png';
-import graphql from '../assets/graphql.png';
-import html from '../assets/html.png';
-import javascript from '../assets/javascript.png';
-import mu5 from '../assets/mu5.png';
-import node from '../assets/node.png';
-import python from '../assets/python.png';
-import react from '../assets/react.png';
-import redux from '../assets/redux.png';
-import sass from '../assets/sass.png';
-import typescript from '../assets/typescript.png';
-import vue from '../assets/vue.png';
+import email from "../assets/email.png";
+import mobile from "../assets/mobile-1.png";
+import api from "../assets/api.png";
+import cpp from "../assets/cpp.png";
+import css from "../assets/css.png";
+import figma from "../assets/figma.png";
+import flutter from "../assets/flutter.png";
+import git from "../assets/git.png";
+import graphql from "../assets/graphql.png";
+import html from "../assets/html.png";
+import javascript from "../assets/javascript.png";
+import mu5 from "../assets/mu5.png";
+import node from "../assets/node.png";
+import python from "../assets/python.png";
+import react from "../assets/react.png";
+import redux from "../assets/redux.png";
+import sass from "../assets/sass.png";
+import typescript from "../assets/typescript.png";
+import vue from "../assets/vue.png";
 
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
-import about04 from '../assets/about04.png';
+import about01 from "../assets/about01.png";
+import about02 from "../assets/about02.png";
+import about03 from "../assets/about03.png";
+import about04 from "../assets/about04.png";
 
-import profile from '../assets/profile.png';
-import circle from '../assets/circle.svg';
-import logo from '../assets/logo.png';
+import profile from "../assets/profile.png";
+import circle from "../assets/circle.svg";
+import logo from "../assets/logo.png";
 import raviLogo from "../assets/ravi-logo-removebg.png";
 import raviProfilePic from "../assets/RaviProfessionalPic.png";
 import location from "../assets/location-icon.png";
@@ -39,13 +39,13 @@ import bootstrap from "../assets/bootstrap.png";
 import resumePdf from "../assets/Ravitheja-Kopparapu-Resume.pdf";
 import downloadIcon from "../assets/download-arrow-icon-1.png";
 
-import adidas from '../assets/adidas.png';
-import amazon from '../assets/amazon.png';
-import asus from '../assets/asus.png';
-import bolt from '../assets/bolt.png';
-import nb from '../assets/nb.png';
-import skype from '../assets/skype.png';
-import spotify from '../assets/spotify.png';
+import adidas from "../assets/adidas.png";
+import amazon from "../assets/amazon.png";
+import asus from "../assets/asus.png";
+import bolt from "../assets/bolt.png";
+import nb from "../assets/nb.png";
+import skype from "../assets/skype.png";
+import spotify from "../assets/spotify.png";
 import gmail from "../assets/gmail.png";
 
 export default {
@@ -93,5 +93,5 @@ export default {
   jsonServer,
   bootstrap,
   resumePdf,
-  downloadIcon
+  downloadIcon,
 };
